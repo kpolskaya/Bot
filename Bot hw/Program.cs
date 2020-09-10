43e4dceb66d90536a951f9fea29784c41d5fcc99
Екатерина Польская <k.polskaya@outlook.com>
2020-09-10 10:49:03 +0300

    Добавьте файлы проекта.

---
+++ Bot hw/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telegram.Bot;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace Bot_hw
+{
+    class Program
+    {
+        static TelegramBotClient bot;
+
+        static void Main(string[] args)
+        {
+
+            string token = File.ReadAllText(@"C:\Users\kpols\Desktop\ДЗ\БОТ\token.txt");
+
+            bot = new TelegramBotClient(token);
+            bot.OnMessage += MessageOn;
+            bot.StartReceiving();
+            Console.ReadKey();
+        }
+        private static void MessageOn(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text} {e.Message.Type}";
+           
+            switch (e.Message.Type.ToString())
+            {
+                case "Text":
+                    if (e.Message.Text!="Covid")
+                    {
+                        bot.SendTextMessageAsync(e.Message.Chat.Id, $"Шлите файл");
+                    }
+                        else
+                        {
+                        try
+                        {
+                            var client = new RestClient("https://covid-19-data.p.rapidapi.com/totals?format=json");
+                            var request = new RestRequest(Method.GET);
+                            request.AddHeader("x-rapidapi-host", "covid-19-data.p.rapidapi.com");
+                            request.AddHeader("x-rapidapi-key", "d93af22c62msh260c18d52dc8569p147315jsn68b6d709f561");
+                            IRestResponse response = client.Execute(request);
+
+                            Newtonsoft.Json.Linq.JArray o = Newtonsoft.Json.Linq.JArray.Parse(response.Content);
+
+                            var confirmed = (string)o[0]["confirmed"];
+                            bot.SendTextMessageAsync(e.Message.Chat.Id,
+                            $"Подтверждено {confirmed} случаев");
+
+                            var recovered = (string)o[0]["recovered"];
+                            bot.SendTextMessageAsync(e.Message.Chat.Id,
+                            $"Выздоровление {recovered} случаев");
+
+                            var critical = (string)o[0]["critical"];
+                            bot.SendTextMessageAsync(e.Message.Chat.Id,
+                            $"В критическом состоянии {critical} случаев");
+
+                            var deaths = (string)o[0]["deaths"];
+                            bot.SendTextMessageAsync(e.Message.Chat.Id,
+                            $"Смерть {deaths} случаев");
+
+                            var lastUpdate = (string)o[0]["lastUpdate"];
+                            bot.SendTextMessageAsync(e.Message.Chat.Id,
+                            $"Обновлено {lastUpdate} ");
+                        }
+                        catch (Exception)
+                        {
+                            bot.SendTextMessageAsync(e.Message.Chat.Id,
+                           $"Ошибка запроса. Covid 19 data center не отвечает.");
+                        }
+                                
+                        }
+                     break;
+                case "Document":
+                    DownLoad(e.Message.Document.FileId, $@"C:\telegram_files\{e.Message.Document.FileName}");
+                    bot.SendTextMessageAsync(e.Message.Chat.Id,
+                    $"Получен файл {e.Message.Document.FileName} сохранен как {$@"C:\telegram_files\{ e.Message.Document.FileName}"}");
+                    break;
+                case "Photo":
+                    string namep = "photo" + Guid.NewGuid();
+                    DownLoad(e.Message.Photo[e.Message.Photo.Length-1].FileId, $@"C:\telegram_files\{namep}.jpg");
+                    bot.SendTextMessageAsync(e.Message.Chat.Id,
+                    $"Получен файл {@"C:\photo.jpg"} сохранен как {namep}.jpg");
+                    break;
+                case "Audio":
+                    string namea = "audio" + Guid.NewGuid();
+                    DownLoad(e.Message.Audio.FileId, $@"C:\telegram_files\{namea}.mp3");
+                    bot.SendTextMessageAsync(e.Message.Chat.Id,
+                    $"Получен файл {e.Message.Audio.Title} сохранен как {namea}.mp3"); 
+                    break;
+
+                case "Video":
+                    DownLoad(e.Message.Video.FileId, $@"C:\telegram_files\{e.Message.Document.FileName}");
+                    bot.SendTextMessageAsync(e.Message.Chat.Id,
+                    $"Получен файл {e.Message.Document.FileName}  сохранен как {e.Message.Document.FileName}");
+                    break;
+                default:
+                    bot.SendTextMessageAsync(e.Message.Chat.Id,
+                   $"Получен файл неизвестного типа. Ошибка записи.");
+                    break;
+            }
+
+        }
+
+        static async void DownLoad(string fileId, string path)
+        {
+            try
+            {
+                var file = await bot.GetFileAsync(fileId);
+                FileStream fs = new FileStream(path, FileMode.Create);
+                await bot.DownloadFileAsync(file.FilePath, fs);
+                fs.Close();
+
+                fs.Dispose();
+
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine("Ошибка загрузки файла: " + ex.Message); 
+            }
+        }
+    }
+}
---
+++ Bot/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создать бота, позволяющего принимать разные типы файлов, 
+            // *Научить бота отправлять выбранный файл в ответ
+            Thread task = new Thread(FH2020bot);
+            task.Start();
+
+        }
+
+        static void FH2020bot()
+        {
+            //string token = "1261212208:AAG7WKfRwyzrCEavawmKBMq_nBFY3boJJvw";
+            string token = File.ReadAllText(@"C:\Users\kpols\Desktop\ДЗ\БОТ\token.txt");
+            WebClient wc = new WebClient() { Encoding = Encoding.UTF8 };
+            int update_id = 0;
+            string startUrl = $@"https://api.telegram.org/bot{token}/";
+
+            while (true)
+            {
+                string url = $"{startUrl}getUpdates?offset={update_id}";
+                var r = wc.DownloadString(url);
+                var msgs = JObject.Parse(r)["result"].ToArray();
+                foreach (dynamic msg in msgs)
+                {
+                    update_id = Convert.ToInt32(msg.update_id) + 1;
+                    string userMessage = msg.message.text;
+                    string userId = msg.message.from.id;
+                    string userFirstName = msg.message.from.first_name;
+
+                    string text = $"{userFirstName} {userId} {userMessage}";
+                    Console.WriteLine(text);
+                    string responseText;
+                    switch (userMessage.ToLower())
+                    {
+                        case "hi":   responseText = $"Hi, {userFirstName}";
+                            break;
+
+                        case "what?":
+                            using (var webClient = new System.Net.WebClient())
+                            {
+                                var json = webClient.DownloadString("https://evilinsult.com/generate_insult.php?lang=en&type=json");
+                                Newtonsoft.Json.Linq.JObject o = Newtonsoft.Json.Linq.JObject.Parse(json);
+                                var value = (string)o["insult"];
+                                responseText = (string)value;
+                                // Console.WriteLine(o);
+                            }
+                            break;
+                        //case "чего?":
+                        //    using (var webClient = new System.Net.WebClient())
+                        //    {
+                        //        var json = webClient.DownloadString("https://evilinsult.com/generate_insult.php?lang=ru&type=json");
+                        //        Newtonsoft.Json.Linq.JObject o = Newtonsoft.Json.Linq.JObject.Parse(json);
+                        //        var value = (string)o["insult"];
+                        //        responseText = (string)value;
+                        //        // Console.WriteLine(o);
+                        //    }
+                        //    break;
+
+                        default:
+                            char[] array = userMessage.ToCharArray();
+                            Array.Reverse(array);
+                            responseText = new string(array);
+                            Console.WriteLine(responseText);
+                            break;
+                    }
+                  
+
+                    url = $"{startUrl}sendMessage?chat_id={userId}&text={responseText}";
+
+                    wc.DownloadString(url);
+                }
+
+                Thread.Sleep(100);
+
+            }
+        }
+
+    }
+
+
+}
